b5614288227852c4244031afbe99d8d2b6629782
Aanchal7915 <aanchal2115@gmail.com>
2025-09-02 21:43:49 +0530

    added jwt secrete in env

--- middleware/auth.js
+++ middleware/auth.js
@@ -9,7 +9,7 @@ exports.authenticate = async (req, res, next) => {
 		return res.status(401).json({ message: 'No token, authorization denied' });
 	}
 	try {
-		const decoded = jwt.verify(token, 'your_jwt_secret');
+		const decoded = jwt.verify(token, process.env.JWT_SECRETE);
 		req.user = await User.findById(decoded.userId).select('-password');
 		if (!req.user) {
 			return res.status(401).json({ message: 'User not found' });
